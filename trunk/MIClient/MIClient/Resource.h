3ba574028d5d69fee54af40b10de8740bb6f1f5c
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-06-18 16:33:32 +0000

    修改资源头文件

--- trunk/MIClient/MIClient/Resource.h
+++ trunk/MIClient/MIClient/Resource.h
@@ -6,13 +6,22 @@
 #define IDD_MICLIENT_DIALOG			102
 #define IDP_SOCKETS_INIT_FAILED		103
 
+#define IDC_EDTREMOTEHOST               1000
+#define IDC_EDTREMOTEPORT               1001
+#define IDC_BTNCONN                     1002
+#define IDC_BTNDISCONN                  1003
+#define IDC_BTNSENDDATA                 1004
+#define IDC_EDTSENDDATA                 1005
+#define IDC_EDTRECV                     1006
+#define IDC_LBLCONNSTATUS               1007
+
 // 新对象的下一组默认值
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 
 #define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_CONTROL_VALUE		1008
 #define _APS_NEXT_SYMED_VALUE		101
 #define _APS_NEXT_COMMAND_VALUE		32771
 #endif
