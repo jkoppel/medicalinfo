14963d5d787fa4dd80e30a6c481bdc8001632b4f
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-06-19 17:30:17 +0000

    重改资源文件

--- MIClient/MIClient/Resource.h
+++ MIClient/MIClient/Resource.h
@@ -1,28 +1,27 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by MIClient.RC
+// Used by MIClient.rc
 //
-#define IDR_MAINFRAME			128
-#define IDD_MICLIENT_DIALOG			102
-#define IDP_SOCKETS_INIT_FAILED		103
+#define IDD_MICLIENT_DIALOG             102
+#define IDR_MAINFRAME                   128
+#define SOCK_ONRECEIVE                  200
+#define SOCK_ONCLOSE                    201
+#define IDP_SOCKETS_INIT_FAILED         1000
+#define IDC_ADDRESS                     1001
+#define IDC_PORT                        1002
+#define IDC_CONNECT                     1003
+#define IDC_NETRECV                     1004
+#define IDC_NETSEND                     1005
+#define IDC_TCP                         1006
+#define IDC_UDP                         1007
 
-#define IDC_EDTREMOTEHOST               1000
-#define IDC_EDTREMOTEPORT               1001
-#define IDC_BTNCONN                     1002
-#define IDC_BTNDISCONN                  1003
-#define IDC_BTNSENDDATA                 1004
-#define IDC_EDTSENDDATA                 1005
-#define IDC_EDTRECV                     1006
-#define IDC_LBLCONNSTATUS               1007
-
-// 新对象的下一组默认值
+// Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1008
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
