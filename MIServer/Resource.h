29c0174d33f7dff4799bb239f7ce968997f27168
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-06-19 15:59:25 +0000

    修改资源定义

--- MIServer/Resource.h
+++ MIServer/Resource.h
@@ -7,6 +7,8 @@
 #define IDP_SOCKETS_INIT_FAILED         103
 #define IDR_MAINFRAME                   128
 #define IDD_CHAT                        129
+#define IDD_SERVERTHREAD                130
+#define IDD_CHATTHREAD                  131
 #define ON_RECEIVE                      200
 #define ON_CLOSE                        201
 #define ON_ACCEPT                       202
@@ -25,7 +27,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           101
