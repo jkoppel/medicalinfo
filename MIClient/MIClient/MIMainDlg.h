76233788f11b202429458a114bec12536b31dcf2
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-06-20 23:59:37 +0000

    添加几个文件到项目

---
+++ MIClient/MIClient/MIMainDlg.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "afxwin.h"
+
+
+// CMIMainDlg dialog
+
+class CMIMainDlg : public CDialog
+{
+	DECLARE_DYNAMIC(CMIMainDlg)
+
+public:
+	CMIMainDlg(CWnd* pParent = NULL);   // standard constructor
+	virtual ~CMIMainDlg();
+
+// Dialog Data
+	enum { IDD = IDD_MICLIENT_MAIN };
+
+protected:
+	void SetConnectStatus(BOOL);
+
+	CString m_strAddress;
+	UINT m_nPort;
+protected:
+	afx_msg LRESULT OnReceive(WPARAM wParam, LPARAM lParam);
+	afx_msg LRESULT OnDisconnect(WPARAM wParam, LPARAM lParam);
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedConnect();
+	virtual BOOL OnInitDialog();
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	afx_msg void OnBnClickedAdd();
+	afx_msg void OnBnClickedEdit();
+	afx_msg void OnBnClickedDelete();
+	afx_msg void OnBnClickedSearch();
+};
