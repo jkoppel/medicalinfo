cce96e11985702d29b6f1d4baa9a6298fe9e5579
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-06-21 00:00:38 +0000

    添加几个文件到项目

---
+++ trunk/MIClient/MIClient/GlobalVars.cpp
@@ -0,0 +1,5 @@
+#include "stdafx.h"
+#include "GlobalVars.h"
+
+CClientSocket * g_pClientSocket = NULL;
+BOOL g_bIsConnected;
---
+++ trunk/MIClient/MIClient/GlobalVars.h
@@ -0,0 +1,11 @@
+#ifndef _GLOBALVARS_H_
+#define _GLOBALVARS_H_
+
+#include "ClientSocket.h"
+
+extern CClientSocket * g_pClientSocket;
+extern BOOL g_bIsConnected;
+
+
+#endif
+
