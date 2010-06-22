6b705333e65217ae9401397347dc89ebf68a6dcb
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-06-22 16:45:41 +0000

    设置连接状态，添加连接命令协议

--- trunk/MIClient/MIClient/MIMainDlg.h
+++ trunk/MIClient/MIClient/MIMainDlg.h
@@ -21,7 +21,6 @@ protected:
 	CString m_strAddress;
 	UINT m_nPort;
 protected:
-	afx_msg LRESULT OnReceive(WPARAM wParam, LPARAM lParam);
 	afx_msg LRESULT OnDisconnect(WPARAM wParam, LPARAM lParam);
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
@@ -30,8 +29,15 @@ public:
 	afx_msg void OnBnClickedConnect();
 	virtual BOOL OnInitDialog();
 	afx_msg void OnTimer(UINT_PTR nIDEvent);
+	void SendCurPacket();
 	afx_msg void OnBnClickedAdd();
 	afx_msg void OnBnClickedEdit();
 	afx_msg void OnBnClickedDelete();
 	afx_msg void OnBnClickedSearch();
+
+	int CmdConnect();
+	int CmdGetRecordNum();
+	int CmdGetRecordAt(int index);
+	int CmdAppendRecord(struct UserData data);
+	int CmdDeleteRecordAt(int index);
 };
