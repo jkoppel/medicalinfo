a91f3bde4acac3e34895621f24050f47f091f77d
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-06-22 16:44:05 +0000

    添加全局函数

--- trunk/MIClient/MIClient/GlobalFuncs.h
+++ trunk/MIClient/MIClient/GlobalFuncs.h
@@ -7,6 +7,9 @@ extern void Char2CString(char *, CString);
 
 //显示消息
 extern void ShowMsg(char msg[]);
+extern void ShowMsg(CString msg);
+//CRC校验
+extern void CRC_16(uchar *input,uchar dlen, uchar *crc);
 
 #endif
 
