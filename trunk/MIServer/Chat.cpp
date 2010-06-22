e42134d0bd0113299bbba32f2acf53e050ce208a
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-06-22 16:39:27 +0000

    添加几个命令接口

--- trunk/MIServer/Chat.cpp
+++ trunk/MIServer/Chat.cpp
@@ -4,6 +4,7 @@
 #include "stdafx.h"
 #include "MIServer.h"
 #include "Chat.h"
+#include "GlobalFuncs.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -74,6 +75,46 @@ LRESULT CChat::OnReceiveData(WPARAM wParam, LPARAM lParam)
 	while(m_peer.GetDataSize() > 0 && m_peer.ReadString(m_sRecv)){
 		GetDlgItem(IDC_RECV)->SetWindowText(m_sRecv);
 	}
+
+	char cmd[100];
+	char buf[1000];
+	int index;
+	int num;
+	struct UserData data;
+	int ret = m_sRecv.GetLength();
+
+	sprintf(cmd, "%s", m_sRecv.GetBuffer(m_sRecv.GetLength()));
+
+	int cmdID = int(cmd[3]-'0');
+	switch(cmdID){
+		case 1:
+			m_peer.Send(CString("OK\r\n"));
+			break;
+		case 2:
+			num = Cmd_GetRecordNum();
+			sprintf(buf, "%s %04d %s", "OK", num, "\r\n");
+			m_peer.Send(CString(buf));
+			break;
+		case 3:
+			sscanf(m_sRecv.GetBuffer(m_sRecv.GetLength()), "%s %04d %s", cmd, &index, buf);
+			Cmd_GetRecordAt(index, data);
+			sprintf(buf, "%s %04d %s %s", "OK", data.id, data.Name, "\r\n");
+			m_peer.Send(CString(buf));
+			break;
+		case 4:
+			sscanf(m_sRecv.GetBuffer(m_sRecv.GetLength()), "%s %04d %s %s", cmd, &data.id, data.Name, buf);
+			Cmd_AppendRecord(data);
+			m_peer.Send(CString("OK\r\n"));
+			break;
+		case 5:
+			sscanf(m_sRecv.GetBuffer(m_sRecv.GetLength()), "%s %04d %s", cmd, &index, buf);
+			Cmd_DeleteRecordAt(index);
+			m_peer.Send(CString("OK\r\n"));
+			break;
+		default:
+			break;
+	}
+
 	return 0;
 }
 
