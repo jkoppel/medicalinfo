6e81548e4ef1e9fd55ae6e03c09330d40989c648
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-06-23 16:49:09 +0000

    修改资源头文件

--- trunk/MIClient/MIClient/Resource.h
+++ trunk/MIClient/MIClient/Resource.h
@@ -2,11 +2,13 @@
 // Microsoft Visual C++ generated include file.
 // Used by MIClient.rc
 //
+#define IDOK2                           2
 #define IDD_MICLIENT_DIALOG             102
 #define IDR_MAINFRAME                   128
 #define IDD_PROGRESS_INFO               129
 #define IDD_CONNECT                     130
 #define IDD_MICLIENT_MAIN               131
+#define IDD_REC_ADDMOD                  132
 #define SOCK_ONRECEIVE                  200
 #define SOCK_ONCLOSE                    201
 #define IDP_SOCKETS_INIT_FAILED         1000
@@ -17,8 +19,11 @@
 #define IDC_NETSEND                     1005
 #define IDC_TCP                         1006
 #define IDC_UDP                         1007
+#define IDC_LIST_PATIENT                1010
 #define IDC_ADD                         1011
+#define IDC_ID                          1011
 #define IDC_EDIT                        1012
+#define IDC_NAME                        1012
 #define IDC_DELETE                      1013
 #define IDC_SEARCH                      1014
 
@@ -26,9 +31,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_RESOURCE_VALUE        134
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
