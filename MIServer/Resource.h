594029dbc0a11a9ef063fe69e5e6387c6fb98319
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-06-24 07:54:26 +0000

    修改资源文件

--- MIServer/Resource.h
+++ MIServer/Resource.h
@@ -9,6 +9,8 @@
 #define IDD_CHAT                        129
 #define IDD_SERVERTHREAD                130
 #define IDD_CHATTHREAD                  131
+#define IDD_PATIENT_LIST                132
+#define IDD_PATIENT_ADDMOD              133
 #define ON_RECEIVE                      200
 #define ON_CLOSE                        201
 #define ON_ACCEPT                       202
@@ -20,16 +22,42 @@
 #define IDC_SERVUDP                     1006
 #define IDC_START                       1007
 #define IDC_BTNSEND                     1008
+#define IDC_TESTDB                      1008
 #define IDC_CLOSE                       1009
 #define IDC_PORT                        1010
+#define IDC_LIST_PATIENT                1011
+#define IDC_ADD                         1012
+#define IDC_EDIT                        1013
+#define IDC_EDIT_SCANCODEID             1013
+#define IDC_DELETE                      1014
+#define IDC_EDIT_NUMBER                 1014
+#define IDC_EDIT_NAME                   1015
+#define IDC_EDIT_SEX                    1016
+#define IDC_EDIT_AGE                    1017
+#define IDC_EDIT_BIRTHDATE              1018
+#define IDC_EDIT_PEOPLE                 1019
+#define IDC_EDIT_DEPARTMENT             1020
+#define IDC_EDIT_TYPEOFWORK             1021
+#define IDC_EDIT_PROVINCE               1022
+#define IDC_EDIT_CITY                   1023
+#define IDC_EDIT_ADDRESS                1024
+#define IDC_EDIT_ZIPCODE                1025
+#define IDC_EDIT_TEL                    1026
+#define IDC_EDIT_CLINICALDIAGNOSIS      1027
+#define IDC_EDIT_HEIGHT                 1028
+#define IDC_EDIT_WEIGHT                 1029
+#define IDC_EDIT_CHECKDATE              1030
+#define IDC_EDIT_HAZARDS                1031
+#define IDC_EDIT_PHARMACY               1032
+#define IDC_EDIT_PASTHISTORY            1033
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_RESOURCE_VALUE        134
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
