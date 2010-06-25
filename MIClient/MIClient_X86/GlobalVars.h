8d8c2840f21c90f877b1c245b84824083839c27a
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-06-25 10:25:28 +0000

    重新定义数据类型

--- MIClient/MIClient_X86/GlobalVars.h
+++ MIClient/MIClient_X86/GlobalVars.h
@@ -19,9 +19,30 @@ extern BOOL g_bIsConnected;
 extern BOOL g_bIsDataComing;
 extern char g_RecvData[1024];
 
+#define MAX_UDATA_STR_LENGTH	50
 struct UserData{
-	int id;
-	char Name[20];
+	int ID;
+	char ScancodeID[MAX_UDATA_STR_LENGTH+1];
+	int Number;
+	char Name[MAX_UDATA_STR_LENGTH+1];
+	char Sex[MAX_UDATA_STR_LENGTH+1];
+	int Age;
+	char BirthDate[MAX_UDATA_STR_LENGTH+1];
+	char People[MAX_UDATA_STR_LENGTH+1];
+	char Department[MAX_UDATA_STR_LENGTH+1];
+	char TypeOfWork[MAX_UDATA_STR_LENGTH+1];
+	char Province[MAX_UDATA_STR_LENGTH+1];
+	char City[MAX_UDATA_STR_LENGTH+1];
+	char Address[MAX_UDATA_STR_LENGTH+1];
+	char ZipCode[MAX_UDATA_STR_LENGTH+1];
+	char Tel[MAX_UDATA_STR_LENGTH+1];
+	char ClinicalDiagnosis[MAX_UDATA_STR_LENGTH+1];
+	int Height;
+	char Weight[MAX_UDATA_STR_LENGTH+1];
+	char CheckDate[MAX_UDATA_STR_LENGTH+1];
+	char Hazards[MAX_UDATA_STR_LENGTH+1];
+	char Pharmacy[MAX_UDATA_STR_LENGTH+1];
+	char PastHistory[MAX_UDATA_STR_LENGTH+1];
 };
 
 #endif
