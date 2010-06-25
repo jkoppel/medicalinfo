ec453ced84f8c93d0851d12852973f1424cae7b7
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-06-25 10:33:19 +0000

    添加主页面对话框文件，及三个从页面对话框文件

---
+++ MIClient/MIClient_X86/PatientTab3.h
@@ -0,0 +1,27 @@
+#pragma once
+
+
+// CPatientTab3 dialog
+
+class CPatientTab3 : public CDialog
+{
+	DECLARE_DYNAMIC(CPatientTab3)
+
+public:
+	CPatientTab3(CWnd* pParent = NULL);   // standard constructor
+	virtual ~CPatientTab3();
+
+// Dialog Data
+	enum { IDD = IDD_PATIENT_TAB3 };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+	DECLARE_MESSAGE_MAP()
+public:
+	CString m_strClinicalDiagnosis;
+public:
+	CString m_strPharmacy;
+public:
+	CString m_strPastHistory;
+};
