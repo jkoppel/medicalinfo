f8a473ccef9b42feef28c36937579f1e2ce568f9
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-06-29 15:45:43 +0000

    修改命令ShowMsg参数，添加函数注释

--- trunk/MIClient/MIClient_X86/GlobalFuncs.h
+++ trunk/MIClient/MIClient_X86/GlobalFuncs.h
@@ -1,15 +1,13 @@
 #ifndef _GLOBALFUNCS_H_
 #define _GLOBALFUNCS_H_
 
-//
 extern void CString2Char(CString, char *);
-extern void Char2CString(char *, CString);
+extern void Char2CString(const char *, CString);
 
 //œ‘ æœ˚œ¢
-extern void ShowMsg(char msg[]);
+extern void ShowMsg(const char *msg);
 extern void ShowMsg(CString msg);
 
-extern CStringList g_strList;
 extern void MakeSeparatorString(CString &destStr);
 extern void ParseSeparatorString(CString str);
 extern void MakeAddOrModRecordCmd(BOOL IsAdd, struct UserData data, CString &str);
