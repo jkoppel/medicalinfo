551b272f6b6d6f3d5c9a20f413254e69af6f7bd8
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-06-29 10:34:18 +0000

    修改命令流程、按钮相应及鼠标拖放响应函数

--- trunk/MIClient/MIClient_X86/MIMainDlg.cpp
+++ trunk/MIClient/MIClient_X86/MIMainDlg.cpp
@@ -28,6 +28,16 @@ CMIMainDlg::CMIMainDlg(CWnd* pParent /*=NULL*/)
 	//m_strAddress = CString(buf);//CString("192.168.0.109");
 	m_nPort = 5000;
 	m_nRecNum = 0;
+	m_bPageMode = TRUE;
+	m_nCurrPageIndex = 0;
+
+	m_bDragging = FALSE;
+	m_nDragIndex = -1;
+	m_nDropIndex = -1;
+	m_pDragImage = NULL;
+	m_pDragList = NULL;
+	m_pDropList = NULL;
+	m_pDropWnd = NULL;
 }
 
 CMIMainDlg::~CMIMainDlg()
@@ -47,7 +57,17 @@ BEGIN_MESSAGE_MAP(CMIMainDlg, CDialog)
 	ON_BN_CLICKED(IDC_ADD, &CMIMainDlg::OnBnClickedAdd)
 	ON_BN_CLICKED(IDC_EDIT, &CMIMainDlg::OnBnClickedEdit)
 	ON_BN_CLICKED(IDC_DELETE, &CMIMainDlg::OnBnClickedDelete)
-END_MESSAGE_MAP()
+	ON_BN_CLICKED(IDC_MOVEPREV, &CMIMainDlg::OnBnClickedMoveprev)
+	ON_BN_CLICKED(IDC_MOVENEXT, &CMIMainDlg::OnBnClickedMovenext)
+	ON_BN_CLICKED(IDC_PAGE_FIRST, &CMIMainDlg::OnBnClickedPageFirst)
+	ON_BN_CLICKED(IDC_PAGE_PREV, &CMIMainDlg::OnBnClickedPagePrev)
+	ON_BN_CLICKED(IDC_PAGE_NEXT, &CMIMainDlg::OnBnClickedPageNext)
+	ON_BN_CLICKED(IDC_PAGE_LAST, &CMIMainDlg::OnBnClickedPageLast)
+	ON_BN_CLICKED(IDC_SWITCH_PAGEMODE, &CMIMainDlg::OnBnClickedSwitchPagemode)
+	ON_NOTIFY(LVN_BEGINDRAG, IDC_LIST_PATIENT, &CMIMainDlg::OnLvnBegindragListPatient)
+	ON_WM_MOUSEMOVE()
+	ON_WM_LBUTTONUP()
+	END_MESSAGE_MAP()
 
 ///接收到断开连接消息
 LRESULT CMIMainDlg::OnDisconnect(WPARAM wParam, LPARAM lParam)
@@ -90,8 +110,7 @@ void CMIMainDlg::OnBnClickedConnect()
 	}
 	
 	///连接服务器
-	int ret, num, index;
-	struct UserData data;
+	int num;
 	CConnectDlg dlg;
 	dlg.m_strAddress = m_strAddress;
 	dlg.m_nPort = m_nPort;
@@ -126,28 +145,15 @@ void CMIMainDlg::OnBnClickedConnect()
 
 				if(CmdConnect()){
 					if(CmdGetRecordNum(num)){
-						int *pID = NULL;
-						pID = new int[num+10];
-						if(CmdGetAllIDs(pID, num)){
-							for(index=0;index<num;index++){
-								ret = CmdGetRecordByID(pID[index], data);
-								if(ret){
-									m_lstPatient.InsertItem(index, CString("病人记录"));
-									UpdateRowData(index, data);
-								}
-								else{
-									delete[]pID;
-									SetConnectStatus(FALSE);
-									ShowMsg("连接失败");
-									return;
-								}
-							}
-							delete []pID;
-							m_nRecNum = num;
+						if(num==0){
+							m_nRecNum = 0;
+							m_lstPatient.DeleteAllItems();
 							ShowMsg("连接成功");
 							return;
 						}
-						delete []pID;
+						UpdateCurrPage();
+						ShowMsg("连接成功");
+						return;
 					}
 				}
 			}
@@ -162,7 +168,7 @@ void CMIMainDlg::OnBnClickedConnect()
 ///设置ListBox项目栏
 void CMIMainDlg::InitListBox()
 {
-	m_lstPatient.SetExtendedStyle(LVS_EX_FULLROWSELECT|LVS_EX_ONECLICKACTIVATE);
+	m_lstPatient.SetExtendedStyle(LVS_EX_FULLROWSELECT|LVS_EX_ONECLICKACTIVATE);//|LVS_EX_HEADERDRAGDROP);
 
 	int i = 0;
 
@@ -241,6 +247,46 @@ void CMIMainDlg::UpdateRowData(int index, struct UserData data)
 
 	UpdateData(FALSE);
 }
+
+void CMIMainDlg::UpdateCurrPage()
+{
+	int ret, index, offset, num, curr_page_size;
+	int *pID = NULL;
+	struct UserData data;
+
+	if(!CmdGetRecordNum(num)){
+		return;
+	}
+	if(num==0){
+		return;
+	}
+	pID = new int[num+10];
+	if(!CmdGetAllIDs(pID, num)){
+		delete []pID;
+		return;
+	}
+
+	if(m_bPageMode){
+		offset = m_nCurrPageIndex * PAGE_SIZE;
+		m_nPageNum = (num+PAGE_SIZE-1)/PAGE_SIZE;
+		curr_page_size = (m_nCurrPageIndex < m_nPageNum - 1) ? PAGE_SIZE : num - (m_nPageNum-1) * PAGE_SIZE;
+	}
+	else{
+		offset = 0;
+		curr_page_size = num;
+	}
+
+	m_lstPatient.DeleteAllItems();
+	for(index=0;index<curr_page_size;index++){
+		ret = CmdGetRecordByID(pID[offset+index], data);
+		if(ret){
+			m_lstPatient.InsertItem(index, CString("病人记录"));
+			UpdateRowData(index, data);
+		}
+	}
+	delete []pID;
+}
+
 
 BOOL CMIMainDlg::OnInitDialog()
 {
@@ -336,17 +382,37 @@ int CMIMainDlg::CmdGetAllIDs(int *pID, int &num)
 		return FALSE;
 	}
 
+	strTmp = g_strList.GetNext(p);//Num
+	if(!p){
+		return FALSE;
+	}
+	if(!strTmp.IsEmpty()){
+		CString2Char(strTmp, buf);
+		sscanf_s(buf, "%d", &num);
+		if(num==0){
+			return TRUE;
+		}
+	}
+
+	int num1 = 0;
 	do{
 		strTmp = g_strList.GetNext(p);//ID
 		if(!strTmp.IsEmpty()){
 			CString2Char(strTmp, buf);
-			sscanf_s(buf, "%d", &pID[num]);
-			num++;
+			if(strncmp(buf, "\r\n", 2)!=0){
+				sscanf_s(buf, "%d", &pID[num1++]);
+			}
+			else{
+				break;
+			}
 		}
 		else{
 			break;
 		}
 	}while(p);
+	if(num!=num1){
+		return FALSE;
+	}
 	
 	return TRUE;
 }
@@ -432,6 +498,129 @@ int CMIMainDlg::CmdModifyRecordByID(int ID, struct UserData data)
 	return TRUE;
 }
 
+int CMIMainDlg::CmdGetNextFreeOrder(int &order)
+{
+	g_bIsDataComing = FALSE;
+	CString cmdStr;
+	char buf[256];
+
+	sprintf_s(buf, "%s", "CMD8||\r\n");
+	g_pClientSocket->Send(buf, (int)strlen(buf));
+
+	g_pClientSocket->Send(cmdStr);
+	if(!ReceiveData()){
+		return FALSE;
+	}
+
+	CString strTmp;
+	POSITION p;
+
+	ParseSeparatorString(CString(g_RecvData));
+	p = g_strList.GetHeadPosition();
+
+	if(!p){
+		return FALSE;
+	}
+
+	g_strList.GetNext(p);//cmd
+	if(!p){
+		return FALSE;
+	}
+
+	strTmp = g_strList.GetNext(p);//order
+	CString2Char(strTmp, buf);
+	sscanf_s(buf, "%d", &order);
+
+	return TRUE;
+}
+
+int CMIMainDlg::CmdGetOrderByID(int ID, int &order)
+{
+	g_bIsDataComing = FALSE;
+	char buf[256];
+	sprintf_s(buf, "%s||%d||%s", "CMD9", ID, "\r\n");
+	g_pClientSocket->Send(buf, (int)strlen(buf));
+	if(!ReceiveData()){
+		return FALSE;
+	}
+
+	CString strTmp;
+	POSITION p;
+
+	ParseSeparatorString(CString(g_RecvData));
+	p = g_strList.GetHeadPosition();
+
+	if(!p){
+		return FALSE;
+	}
+
+	g_strList.GetNext(p);//cmd
+	if(!p){
+		return FALSE;
+	}
+
+	strTmp = g_strList.GetNext(p);//order
+	CString2Char(strTmp, buf);
+	sscanf_s(buf, "%d", &order);
+
+	return TRUE;
+}
+
+int CMIMainDlg::CmdSetOrderByID(int ID, int order)
+{
+	g_bIsDataComing = FALSE;
+	char buf[256];
+	sprintf_s(buf, "%s||%d||%s", "CMDA", ID, "\r\n");
+	g_pClientSocket->Send(buf, (int)strlen(buf));
+	if(!ReceiveData()){
+		return FALSE;
+	}
+
+	CString strTmp;
+	POSITION p;
+
+	ParseSeparatorString(CString(g_RecvData));
+	p = g_strList.GetHeadPosition();
+
+	if(!p){
+		return FALSE;
+	}
+
+	g_strList.GetNext(p);//cmd
+	if(!p){
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+int CMIMainDlg::CmdMoveOrder(int org_order, int dst_order)
+{
+	g_bIsDataComing = FALSE;
+	char buf[256];
+	sprintf_s(buf, "%s||%d||%d||%s", "CMDB", org_order, dst_order, "\r\n");
+	g_pClientSocket->Send(buf, (int)strlen(buf));
+	if(!ReceiveData()){
+		return FALSE;
+	}
+
+	CString strTmp;
+	POSITION p;
+
+	ParseSeparatorString(CString(g_RecvData));
+	p = g_strList.GetHeadPosition();
+
+	if(!p){
+		return FALSE;
+	}
+
+	g_strList.GetNext(p);//cmd
+	if(!p){
+		return FALSE;
+	}
+
+	return TRUE;
+}
 
 void CMIMainDlg::OnBnClickedAdd()
 {
@@ -445,6 +634,10 @@ void CMIMainDlg::OnBnClickedAdd()
 
 		g_ProgressInfo.Show("正在添加...");
 
+		if(!CmdGetNextFreeOrder(data.Order)){
+			ShowMsg("添加失败");
+			return;
+		}
 		ret = CmdAppendRecord(data);
 		if(ret){
 			m_lstPatient.InsertItem(m_nRecNum, CString("病人记录"));
@@ -532,3 +725,290 @@ void CMIMainDlg::OnBnClickedDelete()
 	}
 }
 
+
+void CMIMainDlg::OnBnClickedMoveprev()
+{
+	if(!m_bPageMode){
+		return;
+	}
+}
+
+void CMIMainDlg::OnBnClickedMovenext()
+{
+	if(!m_bPageMode){
+		return;
+	}
+}
+
+void CMIMainDlg::OnBnClickedPageFirst()
+{
+	if(!m_bPageMode){
+		return;
+	}
+	if(m_nCurrPageIndex==0){
+		return;
+	}
+	m_nCurrPageIndex = 0;
+	UpdateCurrPage();
+}
+
+void CMIMainDlg::OnBnClickedPagePrev()
+{
+	if(!m_bPageMode){
+		return;
+	}
+	if(m_nCurrPageIndex==0){
+		return;
+	}
+	m_nCurrPageIndex--;
+	UpdateCurrPage();
+}
+
+void CMIMainDlg::OnBnClickedPageNext()
+{
+	if(!m_bPageMode){
+		return;
+	}
+	if(m_nCurrPageIndex==m_nPageNum-1){
+		return;
+	}
+	m_nCurrPageIndex++;
+	UpdateCurrPage();
+}
+
+void CMIMainDlg::OnBnClickedPageLast()
+{
+	if(!m_bPageMode){
+		return;
+	}
+	if(m_nCurrPageIndex==m_nPageNum-1){
+		return;
+	}
+	m_nCurrPageIndex = m_nPageNum - 1;
+	UpdateCurrPage();
+}
+
+void CMIMainDlg::OnBnClickedSwitchPagemode()
+{
+	m_bPageMode = !m_bPageMode;
+	if(m_bPageMode){
+		m_nCurrPageIndex = 0;
+	}
+	UpdateCurrPage();
+}
+
+void CMIMainDlg::OnLvnBegindragListPatient(NMHDR *pNMHDR, LRESULT *pResult)
+{
+	LPNMLISTVIEW pNMLV = reinterpret_cast<LPNMLISTVIEW>(pNMHDR);
+
+	m_nDragIndex = pNMLV->iItem;
+
+	//// Create a drag image
+	POINT pt;
+	int nOffset = -10; //offset in pixels for drag image
+	pt.x = nOffset;
+	pt.y = nOffset;
+
+	m_pDragImage = m_lstPatient.CreateDragImage(m_nDragIndex, &pt);
+	ASSERT(m_pDragImage);//make sure it was created
+	//We will call delete later (in LButtonUp) to clean this up
+
+	CBitmap bitmap;
+	bitmap.LoadBitmap(IDB_DRAGDROP);
+	m_pDragImage->Replace(0, &bitmap, &bitmap);
+
+	//// Change the cursor to the drag image
+	////	(still must perform DragMove() in OnMouseMove() to show it moving)
+	m_pDragImage->BeginDrag(0, CPoint(nOffset, nOffset - 4));
+	m_pDragImage->DragEnter(GetDesktopWindow(), pNMLV->ptAction);
+
+	//// Set dragging flag and others
+	m_bDragging = TRUE;	//we are in a drag and drop operation
+	m_nDropIndex = -1;	//we don't have a drop index yet
+	m_pDragList = &m_lstPatient; //make note of which list we are dragging from
+	m_pDropWnd = &m_lstPatient;	//at present the drag list is the drop list
+
+	//// Capture all mouse messages
+	SetCapture ();
+
+	*pResult = 0;
+}
+
+void CMIMainDlg::OnMouseMove(UINT nFlags, CPoint point)
+{
+	if (m_bDragging)
+	{
+		//// Move the drag image
+		CPoint pt(point);	//get our current mouse coordinates
+		ClientToScreen(&pt); //convert to screen coordinates
+		m_pDragImage->DragMove(pt); //move the drag image to those coordinates
+		// Unlock window updates (this allows the dragging image to be shown smoothly)
+		m_pDragImage->DragShowNolock(false);
+
+		//// Get the CWnd pointer of the window that is under the mouse cursor
+		CWnd* pDropWnd = WindowFromPoint (pt);
+		ASSERT(pDropWnd); //make sure we have a window
+
+		//// If we drag outside current window we need to adjust the highlights displayed
+		if (pDropWnd != m_pDropWnd)
+		{
+			if (m_nDropIndex != -1) //If we drag over the CListCtrl header, turn off the hover highlight
+			{
+				TRACE("m_nDropIndex is -1\n");
+				CListCtrl* pList = (CListCtrl*)m_pDropWnd;
+				VERIFY (pList->SetItemState (m_nDropIndex, 0, LVIS_DROPHILITED));
+				// redraw item
+				VERIFY (pList->RedrawItems (m_nDropIndex, m_nDropIndex));
+				pList->UpdateWindow ();
+				m_nDropIndex = -1;
+			}
+			else //If we drag out of the CListCtrl altogether
+			{
+				TRACE("m_nDropIndex is not -1\n");
+				CListCtrl* pList = (CListCtrl*)m_pDropWnd;
+				int i = 0;
+				int nCount = pList->GetItemCount();
+				for(i = 0; i < nCount; i++)
+				{
+					pList->SetItemState(i, 0, LVIS_DROPHILITED);
+				}
+				pList->RedrawItems(0, nCount);
+				pList->UpdateWindow();
+			}
+		}
+
+		// Save current window pointer as the CListCtrl we are dropping onto
+		m_pDropWnd = pDropWnd;
+
+		// Convert from screen coordinates to drop target client coordinates
+		pDropWnd->ScreenToClient(&pt);
+
+		//If we are hovering over a CListCtrl we need to adjust the highlights
+		if(pDropWnd->IsKindOf(RUNTIME_CLASS (CListCtrl)))
+		{			
+			//Note that we can drop here
+			SetCursor(LoadCursor(NULL, IDC_ARROW));
+			UINT uFlags;
+			CListCtrl* pList = (CListCtrl*)pDropWnd;
+
+			// Turn off hilight for previous drop target
+			pList->SetItemState (m_nDropIndex, 0, LVIS_DROPHILITED);
+			// Redraw previous item
+			pList->RedrawItems (m_nDropIndex, m_nDropIndex);
+			
+			// Get the item that is below cursor
+			m_nDropIndex = ((CListCtrl*)pDropWnd)->HitTest(pt, &uFlags);
+			// Highlight it
+			pList->SetItemState(m_nDropIndex, LVIS_DROPHILITED, LVIS_DROPHILITED);
+			// Redraw item
+			pList->RedrawItems(m_nDropIndex, m_nDropIndex);
+			pList->UpdateWindow();
+		}
+		else
+		{
+			//If we are not hovering over a CListCtrl, change the cursor
+			// to note that we cannot drop here
+			SetCursor(LoadCursor(NULL, IDC_NO));
+		}
+		// Lock window updates
+		m_pDragImage->DragShowNolock(true);
+	}
+
+	CDialog::OnMouseMove(nFlags, point);
+}
+
+void CMIMainDlg::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	if (m_bDragging)
+	{
+		// Release mouse capture, so that other controls can get control/messages
+		ReleaseCapture ();
+
+		// Note that we are NOT in a drag operation
+		m_bDragging = FALSE;
+
+		// End dragging image
+		m_pDragImage->DragLeave (GetDesktopWindow ());
+		m_pDragImage->EndDrag ();
+		delete m_pDragImage; //must delete it because it was created at the beginning of the drag
+
+		CPoint pt (point); //Get current mouse coordinates
+		ClientToScreen (&pt); //Convert to screen coordinates
+		// Get the CWnd pointer of the window that is under the mouse cursor
+		CWnd* pDropWnd = WindowFromPoint (pt);
+		ASSERT (pDropWnd); //make sure we have a window pointer
+		// If window is CListCtrl, we perform the drop
+		if (pDropWnd->IsKindOf (RUNTIME_CLASS (CListCtrl)))
+		{
+			m_pDropList = (CListCtrl*)pDropWnd; //Set pointer to the list we are dropping on
+			DropItemOnList(m_pDragList, m_pDropList); //Call routine to perform the actual drop
+		}
+	}
+
+	CDialog::OnLButtonUp(nFlags, point);
+}
+
+void CMIMainDlg::DropItemOnList(CListCtrl* pDragList, CListCtrl* pDropList)
+{
+	//This routine performs the actual drop of the item dragged.
+	//It simply grabs the info from the Drag list (pDragList)
+	// and puts that info into the list dropped on (pDropList).
+	//Send:	pDragList = pointer to CListCtrl we dragged from,
+	//		pDropList = pointer to CListCtrl we are dropping on.
+	//Return: nothing.
+
+	////Variables
+
+
+	// Unhilight the drop target
+	pDropList->SetItemState (m_nDropIndex, 0, LVIS_DROPHILITED);
+
+	//Set up the LV_ITEM for retrieving item from pDragList and adding the new item to the pDropList
+	TCHAR szLabel[256];
+	LVITEM lviT;
+
+	ZeroMemory(&lviT, sizeof (LVITEM)); //allocate and clear memory space for LV_ITEM
+	lviT.iItem		= m_nDragIndex;
+	lviT.mask		= LVIF_TEXT;
+	lviT.pszText	= szLabel;
+	lviT.cchTextMax	= 255;
+
+	lvItem lvi;
+	lvi.plvi = &lviT;
+	lvi.plvi->iItem = m_nDragIndex;
+	lvi.plvi->mask = LVIF_TEXT;
+	lvi.plvi->pszText = szLabel;
+	lvi.plvi->cchTextMax = 255;
+
+	// Get item that was dragged
+	pDragList->GetItem (lvi.plvi);
+	lvi.sCol2 = pDragList->GetItemText(lvi.plvi->iItem, 1);
+	CString str = pDragList->GetItemText(lvi.plvi->iItem, 0);
+	char buf[20];
+	int ID;
+	struct UserData data;
+	CString2Char(str, buf);
+	sscanf_s(buf, "%d", &ID);
+	CmdGetRecordByID(ID, data);
+
+	// Delete the original item (for Move operation)
+	// This is optional. If you want to implement a Copy operation, don't delete.
+	// This works very well though for re-arranging items within a CListCtrl.
+	// It is written at present such that when dragging from one list to the other
+	//  the item is copied, but if dragging within one list, the item is moved.
+	if(pDragList == pDropList)
+	{
+		pDragList->DeleteItem (m_nDragIndex);
+		if(m_nDragIndex < m_nDropIndex) m_nDropIndex--; //decrement drop index to account for item
+														//being deleted above it
+	}
+
+	// Insert item into pDropList
+	// if m_nDropIndex == -1, iItem = GetItemCount() (inserts at end of list), else iItem = m_nDropIndex
+	lvi.plvi->iItem = (m_nDropIndex == -1) ? pDropList->GetItemCount () : m_nDropIndex;
+	pDropList->InsertItem (lvi.plvi);
+	UpdateRowData(lvi.plvi->iItem, data);
+
+	// Select the new item we just inserted
+	pDropList->SetItemState (lvi.plvi->iItem, LVIS_SELECTED, LVIS_SELECTED);
+}
--- trunk/MIClient/MIClient_X86/MIMainDlg.h
+++ trunk/MIClient/MIClient_X86/MIMainDlg.h
@@ -7,6 +7,12 @@
 
 // CMIMainDlg dialog
 
+typedef struct {
+	LVITEM* plvi;
+	CString sCol2;
+} lvItem, *plvItem;
+
+
 class CMIMainDlg : public CDialog
 {
 	DECLARE_DYNAMIC(CMIMainDlg)
@@ -17,6 +23,7 @@ public:
 
 // Dialog Data
 	enum { IDD = IDD_MICLIENT_MAIN };
+	enum { PAGE_SIZE = 10 };
 
 protected:
 	void SetConnectStatus(BOOL);
@@ -26,7 +33,18 @@ protected:
 	CListCtrl m_lstPatient;
 	struct UserData m_pUserData[1000];
 	int m_nRecNum;
+	BOOL m_bPageMode;
+	int m_nCurrPageIndex;
+	int m_nPageNum;
 protected:
+	CListCtrl* m_pDragList;		//Which ListCtrl we are dragging FROM
+	CListCtrl* m_pDropList;		//Which ListCtrl we are dropping ON
+	CImageList* m_pDragImage;	//For creating and managing the drag-image
+	BOOL		m_bDragging;	//T during a drag operation
+	int			m_nDragIndex;	//Index of selected item in the List we are dragging FROM
+	int			m_nDropIndex;	//Index at which to drop item in the List we are dropping ON
+	CWnd*		m_pDropWnd;		//Pointer to window we are dropping on (will be cast to CListCtrl* type)
+
 	afx_msg LRESULT OnDisconnect(WPARAM wParam, LPARAM lParam);
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
@@ -41,6 +59,7 @@ public:
 
 	void InitListBox();
 	void UpdateRowData(int index, struct UserData data);
+	void UpdateCurrPage();
 
 	int CmdConnect();
 	int CmdGetRecordNum(int &num);
@@ -49,4 +68,29 @@ public:
 	int CmdAppendRecord(struct UserData &data);
 	int CmdDeleteRecordByID(int ID);
 	int CmdModifyRecordByID(int ID, struct UserData data);
+	int CmdGetNextFreeOrder(int &order);
+	int CmdGetOrderByID(int ID, int &order);
+	int CmdSetOrderByID(int ID, int order);
+	int CmdMoveOrder(int org_order, int dst_order);
+public:
+	afx_msg void OnBnClickedMoveprev();
+public:
+	afx_msg void OnBnClickedMovenext();
+public:
+	afx_msg void OnBnClickedPageFirst();
+public:
+	afx_msg void OnBnClickedPagePrev();
+public:
+	afx_msg void OnBnClickedPageNext();
+public:
+	afx_msg void OnBnClickedPageLast();
+public:
+	afx_msg void OnBnClickedSwitchPagemode();
+public:
+	afx_msg void OnLvnBegindragListPatient(NMHDR *pNMHDR, LRESULT *pResult);
+public:
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+public:
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	void DropItemOnList(CListCtrl* pDragList, CListCtrl* pDropList);
 };
