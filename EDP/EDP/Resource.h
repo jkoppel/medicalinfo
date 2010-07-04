823f15a19fe4cd4acbf2029a77fe17f35442dee2
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-07-04 13:20:03 +0000

    修改资源ID号

--- EDP/EDP/Resource.h
+++ EDP/EDP/Resource.h
@@ -2,24 +2,38 @@
 // Microsoft Visual C++ generated include file.
 // Used by EDP.rc
 //
-#define IDD_ABOUTBOX				100
-#define IDP_OLE_INIT_FAILED			100
-#define IDP_SOCKETS_INIT_FAILED		104
-#define IDR_MAINFRAME				128
-#define IDR_EDPTYPE					129
-#define IDI_CK						130
-#define IDI_BASE_INFO				131
-#define IDI_INPUT					132
-#define IDB_BITMAP_STATE			133
-#define IDB_BITMAP_FOLDER			134
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDD_FORMVIEWEX                  103
+#define IDP_SOCKETS_INIT_FAILED         104
+#define IDR_MAINFRAME                   128
+#define IDR_EDPTYPE                     129
+#define IDI_CK                          130
+#define IDI_BASE_INFO                   131
+#define IDI_INPUT                       132
+#define IDB_BITMAP_STATE                133
+#define IDB_BITMAP_FOLDER               134
+#define IDD_SETTING_DIR                 135
+#define IDB_BITMAP_SETTINGS             136
+#define IDB_BITMAP_DATAFILE             137
+#define IDC_TREE                        1000
+#define IDC_CMB_DIR                     1001
+#define IDC_CMB_TIME                    1002
+#define IDC_TESTDATA                    1003
+#define IDC_BTN_APPLY                   1004
+#define IDC_LIST_DIR                    1005
+#define IDC_LISTBUDDY                   1006
+#define IDC_LIST1                       1015
+#define IDC_TAB2                        1019
+#define ID_SETTINGS                     32772
 
-// 新对象的下一组默认值
+// Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	135
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        139
+#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_CONTROL_VALUE         1020
+#define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
