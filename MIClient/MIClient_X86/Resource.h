cc098a0a22b598f77a3e80023dae9f3f46841792
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-07-08 10:22:44 +0000

    修改资源头文件

--- MIClient/MIClient_X86/Resource.h
+++ MIClient/MIClient_X86/Resource.h
@@ -55,14 +55,18 @@
 #define IDC_SWITCH_PAGEMODE             1043
 #define IDC_SCANCODEID                  1044
 #define IDC_STATUS                      1045
+#define IDC_PAGEMODE                    1046
+#define IDC_PAGEINFO                    1048
+#define IDC_SETTING                     1049
+#define IDC_STATIC_SCANCODE             1050
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_RESOURCE_VALUE        170
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1048
+#define _APS_NEXT_CONTROL_VALUE         1051
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
