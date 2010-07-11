d97ae0b1a152a2fe47dc7297344dbfade17d1329
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-07-11 17:19:02 +0000

    修改资源头文件

--- trunk/MIClient/MIClient/Resource.h
+++ trunk/MIClient/MIClient/Resource.h
@@ -58,14 +58,18 @@
 #define IDC_PAGEINFO                    1048
 #define IDC_SETTING                     1049
 #define IDC_STATIC_SCANCODE             1050
+#define ID_REC_MOVEPREV					32771
+#define ID_REC_MOVENEXT                 32772
+#define ID_REC_MOVETOFIRST              32773
+#define ID_REC_MOVETOLAST				32774
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        170
-#define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1051
+#define _APS_NEXT_RESOURCE_VALUE        171
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1052
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
