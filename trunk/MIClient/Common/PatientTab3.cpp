2ca067c5e7cbae74595c946ca93de90de12a22d7
Huangwenyong <Huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2010-07-12 08:58:00 +0000

    将几个编辑框改为组合框，初始数据从配置文件中读取。

--- trunk/MIClient/Common/PatientTab3.cpp
+++ trunk/MIClient/Common/PatientTab3.cpp
@@ -8,6 +8,8 @@
 #include "..\\MIClient_X86\\MIClient.h"
 #endif
 #include "PatientTab3.h"
+#include "MIBase.h"
+#include "CfgFile.h"
 
 
 // CPatientTab3 dialog
@@ -32,12 +34,14 @@ void CPatientTab3::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
 	DDX_Text(pDX, IDC_EDIT_CLINICALDIAGNOSIS, m_strClinicalDiagnosis);
-	DDX_Text(pDX, IDC_EDIT_PHARMACY, m_strPharmacy);
-	DDX_Text(pDX, IDC_EDIT_PASTHISTORY, m_strPastHistory);
+	DDX_Text(pDX, IDC_COMBO_PHARMACY, m_strPharmacy);
+	DDX_Text(pDX, IDC_COMBO_PASTHISTORY, m_strPastHistory);
 	DDV_MaxChars(pDX, m_strClinicalDiagnosis, 50);
 	DDV_MaxChars(pDX, m_strPharmacy, 50);
 	DDV_MaxChars(pDX, m_strPastHistory, 50);
 	DDX_Control(pDX, IDC_STATUS, m_ctrlStatus);
+	DDX_Control(pDX, IDC_COMBO_PHARMACY, m_ctrlPharmacy);
+	DDX_Control(pDX, IDC_COMBO_PASTHISTORY, m_ctrlPastHistory);
 	DDX_CBIndex(pDX, IDC_STATUS, m_iStatus);
 }
 
@@ -54,6 +58,24 @@ BOOL CPatientTab3::OnInitDialog()
 
 	m_ctrlStatus.InsertString(0, CString("未处理"));
 	m_ctrlStatus.InsertString(1, CString("已处理"));
+
+	char szOptions[40][256];
+	int num, i;
+
+	CfgFile cf;
+	cf.OpenFile(CONFIG_FILE);
+
+	if(cf.GetSectionOptions("MEDICINE", szOptions, 20, num)){
+		for(i=0;i<num;i++){
+			m_ctrlPharmacy.InsertString(i, CString(szOptions[i]));
+		}
+	}
+	if(cf.GetSectionOptions("ANAMNESIS", szOptions, 20, num)){
+		for(i=0;i<num;i++){
+			m_ctrlPastHistory.InsertString(i, CString(szOptions[i]));
+		}
+	}
+	cf.CloseFile();
 
 	return TRUE;  // return TRUE unless you set the focus to a control
 	// EXCEPTION: OCX Property Pages should return FALSE
