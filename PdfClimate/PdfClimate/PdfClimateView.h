98b4430aca7792a3827e0946af56f5dc16dc2e15
huangwenyong <huangwenyong@60799251-6661-9ef8-57ca-37beb366faa2>
2013-04-05 10:32:39 +0000

    implement copy image

--- PdfClimate/PdfClimate/PdfClimateView.h
+++ PdfClimate/PdfClimate/PdfClimateView.h
@@ -57,18 +57,24 @@ protected:
     int m_iZoom;
     int m_iCX;
     int m_iCY;
-
+    CPoint m_ptOrig, m_ptDest;
+    bool m_bDragging;
+    HCURSOR m_hCross;
+    CRect m_rectList[128];
+    int m_iRectNum;
 
 // Generated message map functions
 protected:
     afx_msg void OnFilePrintPreview();
-    afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
     afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
     DECLARE_MESSAGE_MAP()
 public:
     afx_msg void OnSize(UINT nType, int cx, int cy);
     afx_msg void OnChar(UINT nChar, UINT nRepCnt, UINT nFlags);
     afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
+    afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+    afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+    afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
 };
 
 #ifndef _DEBUG  // debug version in PdfClimateView.cpp
